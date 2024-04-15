7580f4a95e3c6940e675b99492067f30bc3e02be
yangyahang <150992210+yangyahang@users.noreply.github.com>
2024-04-15 12:25:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,121 +1,125 @@
-var crs=[]
-var cr
-let color1 = ["#F2EDEC"];
-let color2 = ["#e9c46a", "#e63946", "#f7ede2", "#ffffff", "#582f0e"];
-let gradationpallet1 = [  
-"#7f5539",
+let bg = ["#F2EDEC"];
+let colors=["#e9c46a", "#e63946", "#f7ede2", "#ffffff", "#582f0e"];
+let color1 = [  "#7f5539",
 "#606c38",
 "#ff758f",
 "#f7ede2",
 "#936639",
 "#4ecdc4",
-"#f9c74f",
-];
-let gradationpallet2 = [  
-"#582f0e",
+"#f9c74f",];
+let color2 = [   "#582f0e",
 "#c6ac8f",
 "#fe6d73",
 "#713200",
 "#bd897e",
 "#f9c74f",
 "#6a4c93",
-"#e5383b",
-];
+"#e5383b",];
 
-class cr_class{
+var balls=[]
+var ball
+class ball_class{
   constructor(args){
-    this.p=args.p || {x:random(width),y:random(height)}
-    this.d=args.d || 50
-    this.s1=args.s1 || random(50/3, 50/3)
-    this.s2=args.s2 || random(-50/14, -50/2.5)
-    this.s3=args.s3|| random(50/10)
-    this.v=args.v || {x:random(-2,2),y:random(-2,2)}
-    this.a=args.a || {x:0,y:random(0.7,1.2)}
-    this.c1 = args.c1 || random(color1)
-    this.c2 = args.c2 || random(color2)
-    this.g1 = args.g1 || random(gradationpallet1)
-    this.g2 = args.g2 || random(gradationpallet2)
+    this.p=args.p||{x:width/2,y:height/2}
+    this.r=args.r||random(0.08,0.3)
+    this.c=args.c||random(colors)
+    this.v=args.v||{x:random(-3,3),y:random(-3,3)}
+    this.s=args.s||random(color1)  //邊框
+    this.s1=random(color2) 
+    this.ro=args.ro||random(-20, 20)
+
   }
   draw(){
     push();
-    translate(this.p.x , this.p.y );
-    let gradient = drawingContext.createLinearGradient(
-    this.d,-this.d,this.d,this.d)
-    gradient.addColorStop(0, this.g1);
-    gradient.addColorStop(1, this.g2)
-    noStroke()
-    drawingContext.fillStyle = gradient;
-    arc(0, 0, this.d * 0.9, this.d, 180, 360);
-    circle(0, this.d/8, this.d/2);
-    circle(this.d/3, this.d/8, this.d/2);
-    circle(-this.d/3, this.d/8, this.d/2);
-    fill(this.c2)
-    circle(this.s1,this.s2,this.s3);
-    pop();
-  }
+      translate(this.p.x,this.p.y);
+      angleMode(DEGREES)  //設定為角度
+//------------漸層設定------------            
 
-  update(){
-    this.p.x=this.p.x+this.v.x
-    this.p.y=this.p.y+this.v.y
-    if(this.p.x>width){
-      this.v.x=-this.v.x
+let offset = height / 10;
+let margin = offset / 2;
+let w = (width - offset * 2 - margin * 3) / 4;
+let h = (height - offset * 2 - margin * 3) /4 ;
+
+let d = h;
+      //----------------------------------------      
+      rotate(this.ro);  //旋轉
+//-------漸層--------------------------------      
+      let gradient = drawingContext.createLinearGradient(
+        w / 2,
+        -h / 2,
+        w / 2,
+        h / 2);
+//----------------------------------------------------
+      gradient.addColorStop(0,this.s);
+      gradient.addColorStop(1,this.s1);
+//-----------畫圖-------------------------      
+      noStroke(); 
+      // scale(this.r)
+      drawingContext.fillStyle = gradient;
+      arc(0, 0, d * 0.9, d, 180, 360);
+
+      circle(0, d / 8, d / 2);
+      circle(d / 3, d / 8, d / 2);
+      circle(-d / 3, d / 8, d / 2);
+
+      for (let n = 0; n < d / 5; n++) {
+        fill(random(this.c));
+        circle(
+          random(-d / 3, d / 3),
+          random(-d / 14, -d / 2.5),
+          random(d / 10)
+        );
       }
+    pop();
+  } 
+  update(){  //物件更新後的程式碼
+    this.p.x=this.p.x+this.v.x
+    this.p.y=this.p.y+this.v.y  
+    //反彈
     if(this.p.x<0){
       this.v.x=-this.v.x
-      }
-    if(this.p.y>height){
-      this.v.y= -this.v.y
-      }
+    }
+    if(this.p.x>width){
+      this.v.x=-this.v.x
+    }
     if(this.p.y<0){
-      this.v.y= -this.v.y
-      }
+      this.v.y=-this.v.y
+    }
+    if(this.p.y>height){
+      this.v.y=-this.v.y
+    }
   }
-  isBallInRange(){
+  isballInRange(){  //計算
+    //d:把目前這個物件的位置與滑鼠間的距離
     let d=dist(mouseX,mouseY,this.p.x,this.p.y)
-    if(d<this.d){
+    if(d<this.w){
       return true
-    }
-    else{
+    }else{
       return false
-    }
+    }   
   }
-}  
+}
+
 
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  background(this.c1)
-  for(i=0;i<80;i++){
-    cr=new cr_class({
+  for(i=0;i<30;i=i+1){  //產生幾個
+    ball=new ball_class({ //傳一段參數值到class，以參數為主
+      p:{x:random(0,width),y:random(0,height)}, 
+      v:{x:random(-8,5),y:random(-8,5)}
     })
-   crs.push(cr)
+    balls.push(ball)  //把數據存入
   }
+
 }
-
-
 
 function draw() {
-  background(this.c1)
-
-  for(j=0;j<crs.length;j++){
-    cr=crs[j]
-    cr.draw()
-    cr.update()
-    if(cr.isBallInRange()){
-      cr.v.x=cr.v.x+random(-5,5)
-      cr.v.y=cr.v.y+random(-5,5)
-    }
-    else{
-      cr.v.x=cr.v.x
-      cr.v.y=cr.v.y
-    }
+  background(bg);
+  for(j=0;j<balls.length;j=j+1){
+    ball=balls[j]
+    ball.draw()
+    ball.update()
   }
-}
 
-function mousePressed(){
-  for(let cr of crs){
-    if(cr.isBallInRange()){   
-      crs.splice(crs.indexOf(cr),1)
-    }
-  }
 }
